685846afb76125559343a1cb8c53d80d75264834
Phillip Chen <phillipchen@berkeley.edu>
2025-12-06 11:55:26 -0800

    Added 2 more tags

--- modules/aruco_tags/aruco_tag_generation.py
+++ modules/aruco_tags/aruco_tag_generation.py
@@ -20,7 +20,7 @@ dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
 
 # Generate a marker
 markerImage = np.zeros((200, 200), dtype=np.uint8)
-markerImage_idx = 0 # Valid indices for this dictionary is 0 to 249
+markerImage_idx = 2 # Valid indices for this dictionary is 0 to 249
 border_width = 1 # The number of bits for the thickness of the border; the markers are 6x6 bits, so adding a border of 1 means a 7x7 image
 markerImage = cv.aruco.generateImageMarker(dictionary, markerImage_idx, 200, markerImage, border_width)
 
@@ -29,6 +29,6 @@ markerImage = cv.aruco.generateImageMarker(dictionary, markerImage_idx, 200, mar
 cv.imwrite(save_path + "marker" + str(markerImage_idx) + ".png", markerImage)
 
 # Get a second image
-markerImage_idx = 1 
+markerImage_idx = 3 
 markerImage = cv.aruco.generateImageMarker(dictionary, markerImage_idx, 200, markerImage, border_width)
 cv.imwrite(save_path + "marker" + str(markerImage_idx) + ".png", markerImage)
