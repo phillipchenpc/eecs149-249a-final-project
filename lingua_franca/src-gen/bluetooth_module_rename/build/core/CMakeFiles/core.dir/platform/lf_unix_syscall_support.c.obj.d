53fd19b83fec432e03775514c92ca656b9968b7e
Phillip Chen <phillipchen@berkeley.edu>
2025-12-09 00:37:30 -0800

    Batch delete #9? Getting confused

--- lingua_franca/src-gen/bluetooth_module_rename/build/core/CMakeFiles/core.dir/platform/lf_unix_syscall_support.c.obj.d
+++
@@ -1,2 +0,0 @@
-core/CMakeFiles/core.dir/platform/lf_unix_syscall_support.c.obj: \
- /home/foobar/final/lingua_franca/src-gen/bluetooth_module_rename/core/platform/lf_unix_syscall_support.c
--- lingua_franca/src-gen/bluetooth_module_rename/build/core/CMakeFiles/core.dir/platform/lf_zephyr_support.c.obj.d
+++
@@ -1,2 +0,0 @@
-core/CMakeFiles/core.dir/platform/lf_zephyr_support.c.obj: \
- /home/foobar/final/lingua_franca/src-gen/bluetooth_module_rename/core/platform/lf_zephyr_support.c
